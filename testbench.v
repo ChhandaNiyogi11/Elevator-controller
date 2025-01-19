c620e9f28318a96557d68715537fb36f586ac9b0
CHHANDA NIYOGI <chhanda12000320077@gmail.com>
2025-01-19 17:23:49 +0530

    Update testbench.v

--- testbench.v
+++ testbench.v
@@ -1,26 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 19.01.2025 11:28:12
-// Design Name: 
-// Module Name: tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-`timescale 1ns / 1ps
 
 module tb_elevator_control_4floors;
 
